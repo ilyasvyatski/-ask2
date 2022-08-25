1da5065b7c0072fc68a9814ef26203f5cf4232e2
Alex <63662620+AlexFoxD@users.noreply.github.com>
2022-08-25 12:09:21 +0300

    Initial Commit

---
+++ Тask2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Тask2
+//
+//  Created by neoviso on 8/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

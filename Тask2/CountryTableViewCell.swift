58b19ffd8e324110ce9799cee351b7b42cb10501
Alex <63662620+AlexFoxD@users.noreply.github.com>
2022-08-25 17:33:24 +0300

    task 1 list of countries and search

---
+++ Тask2/CountryTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  CountryTableViewCell.swift
+//  Тask2
+//
+//  Created by neoviso on 8/25/22.
+//
+
+import UIKit
+
+class CountryTableViewCell: UITableViewCell {
+    
+    static let countryCellId = "CountryTableViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Тask2/ViewController.swift
+++ Тask2/ViewController.swift
@@ -9,11 +9,93 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countryTableView: UITableView!
+    @IBOutlet weak var countrySearchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.countrySearchBar.delegate = self
+        self.countryTableView.delegate = self
+        self.countryTableView.dataSource = self
+        
+        getCountriesList()
     }
+    
+    private var countries: [String] = []
+    private var filteredCountries: [String] = []
+    
+    //ведется ли поиск или нет
+    private var isSearching = false
+    
+    func getCountriesList () {
+        countries = Locale.isoRegionCodes.compactMap { Locale.current.localizedString(forRegionCode: $0) }
+        countries.sort()
+    }
+}
 
+extension ViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        //для начала очищаем массив, чтобы не хранить результаты прошлого поиска
+        self.filteredCountries.removeAll()
+        
+        guard !searchText.isEmpty || searchText != " " else {
+            return
+        }
+        
+        for item in countries {
+            let countryName = searchText.lowercased()
+            let isArrayContain = item.lowercased().range(of: countryName)
+            
+            if isArrayContain != nil {
+                filteredCountries.append(item)
+            }
+        }
+        
+        if searchBar.text == ""  {
+            isSearching = false
+            countryTableView.reloadData()
+        } else {
+            isSearching = true
+            filteredCountries = countries.filter({ $0.contains(searchBar.text ?? "") })
+            countryTableView.reloadData()
+        }
+    }
+}
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if isSearching {
+            return filteredCountries.count
+        } else {
+            return countries.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CountryTableViewCell.countryCellId, for: indexPath) as? CountryTableViewCell  else {
+            fatalError()
+        }
+        
+        if isSearching {
+            cell.textLabel?.text = filteredCountries[indexPath.row]
+        } else {
+            cell.textLabel?.text = countries[indexPath.row]
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if isSearching {
+            if let url = URL(string: "https://en.wikipedia.org/wiki/\(filteredCountries.map { $0.replacingOccurrences(of: "\\s+", with: "_", options: .regularExpression) }[indexPath.row])") {
+                        UIApplication.shared.open(url, options: [:])
+                    }
+        } else {
+            if let url = URL(string: "https://en.wikipedia.org/wiki/\(countries.map { $0.replacingOccurrences(of: "\\s+", with: "_", options: .regularExpression) }[indexPath.row])") {
+                        UIApplication.shared.open(url, options: [:])
+                    }
+        }
+    }
 }
 
